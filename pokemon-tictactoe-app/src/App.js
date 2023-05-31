75ffc47ad9dc5dc4dfc60f5f502952232f3f0429
DESKTOP-DJK1QF7\akito <akitochu@gmail.com>
2023-05-31 14:54:09 +0100

    random typings without duplicates displayed at column and row headers

--- pokemon-tictactoe-app/src/App.js
+++ pokemon-tictactoe-app/src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import {useState, useEffect, useReducer} from "react";
 
 const URL = `https://pokeapi.co/api/v2/pokemon/`
+const types = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", 
+               "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy" ]
+
+var gameTypes = ["normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", 
+                "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy" ]
 
 function App() {
   const [tile1, setTile1] = useState("")
@@ -14,6 +19,21 @@ function App() {
   const [tile8, setTile8] = useState("")
   const [tile9, setTile9] = useState("")
   const [PLAYER_ONE_TURN, setTurn] = useState(true);
+
+  const [type1, setType1] = useState(gameTypes[Math.floor(Math.random() * gameTypes.length)])
+  gameTypes.splice(gameTypes.indexOf(type1), 1)
+  const [type2, setType2] = useState(gameTypes[Math.floor(Math.random() * gameTypes.length)])
+  gameTypes.splice(gameTypes.indexOf(type1), 1)
+  const [type3, setType3] = useState(gameTypes[Math.floor(Math.random() * gameTypes.length)])
+  gameTypes.splice(gameTypes.indexOf(type1), 1)
+  const [type4, setType4] = useState(gameTypes[Math.floor(Math.random() * gameTypes.length)])
+  gameTypes.splice(gameTypes.indexOf(type1), 1)
+  const [type5, setType5] = useState(gameTypes[Math.floor(Math.random() * gameTypes.length)])
+  gameTypes.splice(gameTypes.indexOf(type1), 1)
+  const [type6, setType6] = useState(gameTypes[Math.floor(Math.random() * gameTypes.length)])
+  gameTypes.splice(gameTypes.indexOf(type1), 1)
+
+
 
   useEffect(() => {
     if(checkWin()){
@@ -49,52 +69,81 @@ function App() {
   }
 
 
+
   return (
     <div className="App">
       <header className="App-header">
-        <div id="grid">
-          <div id="gridRow1">
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile1("X") : setTile1("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile1}</button>
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile2("X") : setTile2("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile2}</button>
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile3("X") : setTile3("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile3}</button>
+        <div id="gameContainer">
+        <div id="rowHeader">    
+          <div>
+            {type4}
           </div>
-          <div id="gridRow2">
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile4("X") : setTile4("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile4}</button>
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile5("X") : setTile5("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile5}</button>
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile6("X") : setTile6("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile6}</button>
+          <div>
+            {type5}
           </div>
-          <div id="gridRow3">
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile7("X") : setTile7("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile7}</button>
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile8("X") : setTile8("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile8}</button>
-            <button onClick={event => {
-              PLAYER_ONE_TURN ? setTile9("X") : setTile9("O");
-              setTurn(!PLAYER_ONE_TURN);
-            }}>{tile9}</button>
+          <div>
+            {type6}
           </div>
+        </div>
+        
+        <div id="columnHeaderAndGrid">
+          <div id="columnHeader"> 
+            <div>
+              {type1}
+            </div>
+            <div>
+              {type2}
+            </div>
+            <div>
+              {type3}
+            </div>
+          </div>
+          <div id="grid">
+            <div id="gridRow1">
+              <button onClick={event => {
+                PLAYER_ONE_TURN ? setTile1("X") : setTile1("O");
+                setTurn(!PLAYER_ONE_TURN);
+              }}>{tile1}</button>
+              <button onClick={event => {
+                PLAYER_ONE_TURN ? setTile2("X") : setTile2("O");
+                setTurn(!PLAYER_ONE_TURN);
+              }}>{tile2}</button>
+              <button onClick={event => {
+                PLAYER_ONE_TURN ? setTile3("X") : setTile3("O");
+                setTurn(!PLAYER_ONE_TURN);
+              }}>{tile3}</button>
+              </div>
+              <div id="gridRow2">
+                <button onClick={event => {
+                  PLAYER_ONE_TURN ? setTile4("X") : setTile4("O");
+                  setTurn(!PLAYER_ONE_TURN);
+                }}>{tile4}</button>
+                <button onClick={event => {
+                  PLAYER_ONE_TURN ? setTile5("X") : setTile5("O");
+                  setTurn(!PLAYER_ONE_TURN);
+                }}>{tile5}</button>
+                <button onClick={event => {
+                  PLAYER_ONE_TURN ? setTile6("X") : setTile6("O");
+                  setTurn(!PLAYER_ONE_TURN);
+                }}>{tile6}</button>
+              </div>
+              <div id="gridRow3">
+                <button onClick={event => {
+                  PLAYER_ONE_TURN ? setTile7("X") : setTile7("O");
+                  setTurn(!PLAYER_ONE_TURN);
+                }}>{tile7}</button>
+                <button onClick={event => {
+                  PLAYER_ONE_TURN ? setTile8("X") : setTile8("O");
+                  setTurn(!PLAYER_ONE_TURN);
+                }}>{tile8}</button>
+                <button onClick={event => {
+                  PLAYER_ONE_TURN ? setTile9("X") : setTile9("O");
+                  setTurn(!PLAYER_ONE_TURN);
+                }}>{tile9}</button>
+              </div>
+            </div>  
+        </div>
+        
         </div>
         
       </header>
